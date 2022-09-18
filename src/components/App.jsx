73db0f421c15242a3e14c8fd719a5a4954171e24
valery-chumak <blossssom88@gmail.com>
2022-09-18 13:03:04 +0300

    summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,8 @@
+import Feedback from "./Feedback";
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      My test react app
-    </div>
+    <Feedback></Feedback>
   );
 };
